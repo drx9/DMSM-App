b221e61af57c5ab88af9213ec0b119ccf875a0ca
drx9 <darshanb1@outlook.com>
2025-06-29 11:49:45 +0530

    resolve merge conflict

--- dms-backend/src/index.js
+++ dms-backend/src/index.js
@@ -7,7 +7,7 @@ const { errorHandler } = require('./middleware/errorHandler');
 const { syncModels } = require('./models');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
