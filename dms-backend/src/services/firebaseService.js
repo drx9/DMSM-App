fa86508760cc8aed77f877f201555c2dd4bf0a74
drx9 <darshanb1@outlook.com>
2025-07-25 19:01:30 +0530

    added OTP for registeringg

--- dms-backend/src/services/firebaseService.js
+++ dms-backend/src/services/firebaseService.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(require("../../path/to/serviceAccountKey.json")),
+    credential: admin.credential.cert(require("./serviceAccountKey.json")),
   });
 }
 
