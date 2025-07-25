58e0df8d3309eef9bbdac0da15c9d47169749498
drx9 <darshanb1@outlook.com>
2025-07-25 19:07:55 +0530

    Use Railway secret for Firebase Admin

--- dms-backend/src/services/firebaseService.js
+++ dms-backend/src/services/firebaseService.js
@@ -1,8 +1,9 @@
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY);
   admin.initializeApp({
-    credential: admin.credential.cert(require("./serviceAccountKey.json")),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
